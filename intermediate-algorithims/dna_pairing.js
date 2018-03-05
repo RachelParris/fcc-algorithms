8d9297c523fb8b56c954d9e27d5dd88d5d868c18
RachelParris <sonnet22@gmail.com>
2018-03-05 17:42:38 -0500

    Added comments

--- intermediate-algorithims/dna_pairing.js
+++ intermediate-algorithims/dna_pairing.js
@@ -1,8 +1,13 @@
 function pairElement(str) {
   let dna = [];
+
+  // Split string into an array
   str = str.split("");
   
+  // Interate over each letter in the array
   str.map((letter) => {
+
+    // For each letter push the corresponding DNA pairing to the dna array 
     switch(letter) {
       case "A":
         dna.push(["A","T"]);
